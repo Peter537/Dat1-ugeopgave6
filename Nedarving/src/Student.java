71a8388410a7d0e0144502b313f0cb718a773d44
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-13 08:28:15 +0200

    3.3

--- Nedarving/src/Student.java
+++ Nedarving/src/Student.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class Student extends Person {
+
+    private final ArrayList<String> passedCourses;
+    private final ArrayList<String> currentCourses;
 
     public Student(String name) {
         super(name);
+        this.passedCourses = new ArrayList<>();
+        this.currentCourses = new ArrayList<>();
     }
 
 }
--- Nedarving/src/Teacher.java
+++ Nedarving/src/Teacher.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class Teacher extends Person {
 
-    public Teacher(String name) {
+    private final ArrayList<String> canTeach;
+    private final ArrayList<String> currentCourses;
+
+    public Teacher(String name, ArrayList<String> canTeach) {
         super(name);
+        this.canTeach = canTeach;
+        this.currentCourses = new ArrayList<>();
     }
 
 }
